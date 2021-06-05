61d085d1ced6568b115921381cf8de99cc9e02be
ByteZ1337 <0xbytez@gmail.com>
2021-06-05 19:22:52 +0200

    Another license update

--- src/main/java/xyz/xenondevs/particle/data/texture/BlockTexture.java
+++ src/main/java/xyz/xenondevs/particle/data/texture/BlockTexture.java
@@ -1,8 +1,7 @@
 /*
+ * MIT License
  *
  * Copyright (c) 2021 ByteZ1337
- *
- * The MIT License (MIT)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +20,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
  */
 
 package xyz.xenondevs.particle.data.texture;
