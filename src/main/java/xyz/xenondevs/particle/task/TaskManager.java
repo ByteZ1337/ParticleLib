aefff8fec12692b091e40f2f092e949f56c2075e
ByteZ1337 <0xbytez@gmail.com>
2021-06-11 21:55:09 +0200

    Added static wrapper methods to make starting tasks easier
    New methods:
     * TaskManager#startGlobalTask(List<Object> packets, int tickDelay)
     * TaskManager#startTargetedTask(List<Object> packets, int tickDelay, Collection<Player> targets)
     * TaskManager#startSingularTask(List<Object> packets, int tickDelay, UUID target)
     * TaskManager#startSingularTask(List<Object> packets, int tickDelay, Player target)
     * TaskManager#startFilteredTask(List<Object> packets, int tickDelay, Predicate<Player> filter)
     * TaskManager#startSuppliedTask(List<Object> packets, int tickDelay, Supplier<Collection<Player>> supplier)

--- src/main/java/xyz/xenondevs/particle/task/TaskManager.java
+++ src/main/java/xyz/xenondevs/particle/task/TaskManager.java
@@ -25,11 +25,16 @@
 package xyz.xenondevs.particle.task;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import xyz.xenondevs.particle.utils.ParticleUtils;
 import xyz.xenondevs.particle.utils.ReflectionUtils;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * A manager to handle different {@link ParticleTask ParticleTasks}
@@ -53,7 +58,7 @@ public final class TaskManager {
      * Starts a new Timer for the given task.
      *
      * @param task the task that should be added to the scheduler
-     * @return the id of the BukkitTask which can be canceled using {@link TaskManager#stopTask(int)}
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
      * @see TaskManager#stopTask(int)
      */
     public int startTask(ParticleTask task) {
@@ -86,4 +91,88 @@ public final class TaskManager {
     public static TaskManager getTaskManager() {
         return INSTANCE;
     }
+    
+    /**
+     * Starts a new {@link GlobalTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see GlobalTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startGlobalTask(List<Object> packets, int tickDelay) {
+        return getTaskManager().startTask(new GlobalTask(packets, tickDelay));
+    }
+    
+    /**
+     * Starts a new {@link TargetedTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @param targets   A {@link Collection} of {@link Player Players} that will receive the particles.
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see TargetedTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startTargetedTask(List<Object> packets, int tickDelay, Collection<Player> targets) {
+        return getTaskManager().startTask(new TargetedTask(packets, tickDelay, targets));
+    }
+    
+    /**
+     * Starts a new {@link SingularTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @param target    The {@link UUID} of the target {@link Player}
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see SingularTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startSingularTask(List<Object> packets, int tickDelay, UUID target) {
+        return getTaskManager().startTask(new SingularTask(packets, tickDelay, target));
+    }
+    
+    /**
+     * Starts a new {@link SingularTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @param target    The target {@link Player}
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see SingularTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startSingularTask(List<Object> packets, int tickDelay, Player target) {
+        return getTaskManager().startTask(new SingularTask(packets, tickDelay, target));
+    }
+    
+    /**
+     * Starts a new {@link FilteredTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @param filter    The {@link Predicate} to filter the {@link Player Players}
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see FilteredTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startFilteredTask(List<Object> packets, int tickDelay, Predicate<Player> filter) {
+        return getTaskManager().startTask(new FilteredTask(packets, tickDelay, filter));
+    }
+    
+    /**
+     * Starts a new {@link SuppliedTask}.
+     *
+     * @param packets   {@link List} of packets
+     * @param tickDelay The delay of ticks between each execution
+     * @param supplier  The {@link Supplier} used to retrieve the {@link Collection} of target {@link Player Players}
+     * @return the id of the BukkitTask which can be cancelled using {@link TaskManager#stopTask(int)}
+     * @see SuppliedTask
+     * @see TaskManager#stopTask(int)
+     */
+    public static int startSuppliedTask(List<Object> packets, int tickDelay, Supplier<Collection<Player>> supplier) {
+        return getTaskManager().startTask(new SuppliedTask(packets, tickDelay, supplier));
+    }
+    
 }
